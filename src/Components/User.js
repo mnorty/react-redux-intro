21264a9cb238a57fb23b28a66eac5a877155c6e3
mnorty <mnorty@gmail.com>
2019-05-29 10:37:04 -0600

    displaying h1 on page

---
+++ src/Components/User.js
@@ -0,0 +1,13 @@
+import React , {Component} from 'react'
+import {getUser} from '../redux/userReducer'
+import {connect} from 'react-redux'
+
+class User extends Component {
+  render(){
+    return (
+        <h1> User</h1>
+    )
+  }
+}
+
+export default User
